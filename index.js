bd1516760f4925331b6e1d4ebf03c73653be052e
dlimla <danielhlim1990@gmail.com>
2019-03-27 19:43:33 -0700

    inits project, migrations, db, POST login and registers user

---
+++ index.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
+const bcrypt = require('bcrypt');
+
+const db = require('./data/dbConfig.js');
+const Users = require('./users/users-model.js');
+
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+server.use(cors());
+
+server.get('/', (req, res) => {
+  res.send("It's alive!");
+});
+
+server.post('/api/register', (req, res) => {
+  let user = req.body;
+  // console.log('user before hash', user)
+  const hash = bcrypt.hashSync(user.password, 10)
+  user.password = hash;
+  // console.log('user post hash', user)
+
+  Users.add(user)
+    .then(saved => {
+      res.status(200).json(saved);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+
+})
+
+
+server.post('/api/login', (req, res) => {
+  let { username, password } = req.body;
+
+  Users.findBy({username})
+  .first()
+  .then(user => {
+    if(user && bcrypt.compareSync(password, user.password)){
+      res.status(200).json({ message: `Welcome ${user.username}!` })
+    }
+    else {
+      res.status(401).json({ message: 'Invalid Credentials' })
+    }
+  })
+  .catch(error => {
+    res.status(500).json({ message: 'User not found' });
+  }) 
+
+})
+
+
+server.get('/api/users', (req, res) => {
+
+  Users.find()
+    .then(users => {
+      res.json(users);
+    })
+    .catch(err => res.send(err));
+});
+
+const port = 5000;
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+
+
